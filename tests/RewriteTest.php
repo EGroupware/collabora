92038e8be870b491539af78337a43d4ac87327d1
nathangray <nathangray.bsc+github@gmail.com>
2017-10-20 09:39:16 +0200

    Update URLs in test to match how we're doing them now (no redirect, handle it in index.php)

--- tests/RewriteTest.php
+++ tests/RewriteTest.php
@@ -15,24 +15,16 @@ namespace EGroupware\collabora;
 // test base providing Egw environment, since we need the DB
 require_once realpath(__DIR__.'/../../api/tests/LoggedInTest.php');
 
-use EGroupware\Api;
-use EGroupware\Api\Vfs\Sharing;
+use EGroupware\Api\Egw;
 
 class RewriteTest extends \EGroupware\Api\LoggedInTest {
-
-	protected static $wopi_endpoint;
-
-	public function setUp()
-	{
-		static::$wopi_endpoint = Api\Egw::link( Bo::WOPI_ENDPOINT );
-	}
 
 	/**
 	 * Try something invalid, make sure it fails
 	 */
 	public function testInvalidUrl()
 	{
-		$url = static::$wopi_endpoint . 'files/totally_invalid';
+		$url = Egw::link('/collabora/index.php/wopi/files/totally_invalid');
 		$headers = get_headers($url, TRUE);
 		$this->assertEquals('404', substr($headers[0], 9, 3), "Testing invalid URL $url");
 	}
@@ -47,7 +39,7 @@ class RewriteTest extends \EGroupware\Api\LoggedInTest {
 		$token = Bo::get_token('/home');
 
 		// home dir gets ID 2 normally
-		$url = static::$wopi_endpoint . 'files/2?access_token=' . urlencode($token['token']);
+		$url = Egw::link('/collabora/index.php/wopi/files/2?access_token=' . urlencode($token['token']));
 
 		$headers = get_headers($url, TRUE);
 		$this->assertEquals('200', substr($headers[0], 9, 3), "Testing home directory $url");
