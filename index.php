06bc8ea996f9c54caa1e5205da7842b5af186c91
ralf <rb@egroupware.org>
2022-04-01 11:24:43 +0300

    do NOT require filemanager rights for WOPI endpoint, access-control is through tokens anyway

--- index.php
+++ index.php
@@ -14,13 +14,13 @@ $GLOBALS['egw_info'] = array(
 		'disable_Template_class' => true,
 		'noheader'  => true,
 		'nonavbar' => 'always',	// true would cause eTemplate to reset it to false for non-popups!
-		'currentapp' => 'filemanager',
+		'currentapp' => 'api',
 		'autocreate_session_callback' => 'EGroupware\\collabora\\Wopi::create_session',
 		'no_exception_handler' => 'basic_auth',	// we use a basic auth exception handler (sends exception message as basic auth realm)
 	)
 );
 
-include_once '../header.inc.php';
+require_once '../header.inc.php';
 
 require_once EGW_INCLUDE_ROOT.'/collabora/src/Wopi.php';
 \EGroupware\collabora\Wopi::index();
