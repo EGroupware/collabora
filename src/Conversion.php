00a0e67530c84329364aafe44f6db764639e72f0
ralf <rb@egroupware.org>
2022-06-10 10:54:11 +0200

    allow to use regular filesystem for conversation, default is Vfs

--- src/Conversion.php
+++ src/Conversion.php
@@ -4,6 +4,7 @@ namespace EGroupware\Collabora;
 
 use EGroupware\Api\Exception\NotFound;
 use EGroupware\Api\Json\Response;
+use EGroupware\Api\MimeMagic;
 use EGroupware\Api\Vfs;
 
 class Conversion
@@ -41,9 +42,22 @@ class Conversion
 		Response::get()->data($data);
 	}
 
-	public function convert($file, &$destination, $to_format, &$error_message)
+	/**
+	 * @param string $file
+	 * @param string& $destination
+	 * @param string $to_format extension, eg. 'pdf'
+	 * @param string& $error_message
+	 * @param bool $use_vfs true: use EGroupware VFS (eg. add Vfs::PREFIX), false: use regular filesystem eg. /tmp
+	 * @return bool
+	 * @throws NotFound
+	 * @throws Vfs\Exception\ProtectedDirectory
+	 * @throws \EGroupware\Api\Exception\AssertionFailed
+	 * @throws \EGroupware\Api\Exception\WrongParameter
+	 */
+	public function convert($file, &$destination, $to_format, &$error_message, $use_vfs=true)
 	{
-		if(!Vfs::file_exists($file) || !Vfs::is_readable($file))
+		if ($use_vfs && (!Vfs::file_exists($file) || !Vfs::is_readable($file)) ||
+			!$use_vfs && (!file_exists($file) || !is_readable($file)))
 		{
 			throw new NotFound("Entry not found: $file");
 		}
@@ -76,14 +90,15 @@ class Conversion
 		*/
 
 		$curl_session = curl_init();
-		$curl_data = ['data' => new \CURLFile(Vfs::PREFIX . $file, Vfs::mime_content_type($file), $filename)];
+		$curl_data = ['data' => new \CURLFile(($use_vfs ? Vfs::PREFIX : '') . $file,
+			$use_vfs ? Vfs::mime_content_type($file) : MimeMagic::filename2mime($file), $filename)];
 
 		curl_setopt($curl_session, CURLOPT_URL, $url);
 		curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curl_session, CURLOPT_POST, 1);
 		curl_setopt($curl_session, CURLOPT_POSTFIELDS, $curl_data);
 		curl_setopt($curl_session, CURLOPT_HEADER, false);
-		$result = file_put_contents(Vfs::PREFIX . $destination, curl_exec($curl_session));
+		$result = file_put_contents(($use_vfs ? Vfs::PREFIX : '') . $destination, curl_exec($curl_session));
 		if(curl_errno($curl_session))
 		{
 			$error_message = curl_error($curl_session);
@@ -92,7 +107,7 @@ class Conversion
 
 		if(!$result)
 		{
-			Vfs::remove($destination);
+			$use_vfs ? Vfs::remove($destination) : unlink($destination);
 		}
 		return $result > 0;
 	}
